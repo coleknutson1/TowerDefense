23a2299c8e665417300c6a4a7d15425f57a32346
coleknutson1 <cole.knutson1@gmail.com>
2016-02-12 20:57:35 -0600

    Got the clicker working!

--- TowerDefenseLogic.js
+++ TowerDefenseLogic.js
@@ -1,4 +1,29 @@
-//Test
-var myFunction = function(){
-	var x = "Hello";
+
+function init()
+{		
+ 	var stage = new createjs.Stage("demoCanvas");
+    var circle = new createjs.Shape();
+    window.myCount = 0;
+    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+    circle.x = 100;
+    circle.y = 100;
+    stage.addChild(circle);    
+
+	var text = new createjs.Text();
+	text.text = window.myCount;
+	stage.addChild(text);
+	circle.addEventListener("click", 
+		function(event) 
+		{ 
+			window.myCount+=1; 
+			text.text=window.myCount;
+			stage.update();
+		});
+	stage.update();
 }
+
+
+// window.addListeners = function(circle)
+// {
+	
+// }
